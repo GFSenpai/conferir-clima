2d57a3145830d1127463eaaa5f04f53a898f664c
Gabriel Faria <gabriel10gfs10@gmail.com>
2023-04-01 02:25:23 -0300

    second commit

--- script.js
+++ script.js
@@ -1,50 +1,44 @@
-const apikey = "3265874a2c77ae4a04bb96236a642d2f";
+const input = document.querySelector("input");
+const button = document.querySelector("button");
 
-const main = document.getElementById("main");
-const form = document.getElementById("form");
-const search = document.getElementById("search");
+const place = document.querySelector("#place");
+const degrees = document.querySelector("#degrees");
+const img = document.querySelector("img");
+const wind = document.querySelector("#wind");
+const content = document.querySelector(".content");
 
-const url = (city) =>
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+button.addEventListener("click", () => {
+  if (!input.value) return;
 
-async function getWeatherByLocation(city) {
-    const resp = await fetch(url(city), { origin: "cors" });
-    const respData = await resp.json();
-
-    console.log(respData);
-
-    addWeatherToPage(respData);
-}
-
-function addWeatherToPage(data) {
-    const temp = KtoC(data.main.temp);
-
-    const weather = document.createElement("div");
-    weather.classList.add("weather");
-
-    weather.innerHTML = `
-        <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
-        <small>${data.weather[0].main}</small>
-    `;
-
-    // cleanup
-    main.innerHTML = "";
-
-    main.appendChild(weather);
-}
-
-function KtoC(K) {
-    return Math.floor(K - 273.15);
-}
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const city = search.value;
-
-    if (city) {
-        getWeatherByLocation(city);
-    }
+  getDataApi();
 });
+
+async function getDataApi() {
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
+    input.value
+  )}&units=metric&appid=84902c31c027fc4ccb7fc86c47eadb75`;
+
+  try {
+    await fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data?.cod && data.cod === "404") {
+          return alert("Local não encontrado!");
+        }
+
+        loadData(data);
+      });
+  } catch (error) {
+    alert(error);
+  }
+}
+
+function loadData(data) {
+  place.innerHTML = `${data.name}, ${data.sys.country}`;
+  degrees.innerHTML = `Temperatura: ${Math.floor(data.main.temp)}° C`;
+  img.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  wind.innerHTML = `Vento: ${data.wind.speed} km/h`;
+  content.style.display = "flex";
+}
 
 
